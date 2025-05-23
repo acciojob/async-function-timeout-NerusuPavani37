7080c5204507eba00684e0b322d38e2005f51f7e
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-05-23 21:14:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+function display (text,delay){
+   return new Promise ((resolve,reject)=>{
+    setTimeout(()=>{
+      let output = document.getElementById('output');
+      output.innerHTML += text;
+      resolve()
+   },Number(delay))
+   })
+    
+}
+
+async function handleSumbit(){
+  let form = document.querySelector('form');
+   let mess = document.getElementById('text').value;
+   let delay = document.getElementById('delay').value;
+   await display(mess,delay);
+   form.reset()
+}
+
+
+document.getElementById('btn').addEventListener('click' , handleSumbit);
